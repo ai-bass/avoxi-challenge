4806c69174ad4b0b9a943b86691e31d1c97b9746
unknown <ai_billyg_stan@outlook.com>
2022-05-29 15:10:02 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # avoxi-challenge
 AVOXI coding challenge
+
+Run API:
+
+>pip3 install -r requirements.txt
+>python -m flask run --host=0.0.0.0
+
+Run Docker:
+>docker build --tag python-docker .
+>docker run -d -p 5000:5000 python-docker
+
+
---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+import geoip2.database
+
+# This reader object should be reused across lookups as creation of it is
+# expensive.
+reader = geoip2.database.Reader('GeoLite2-Country.mmdb')
+
+#Default route explains how to run ip check
+@app.route('/')
+def hello_geek():
+    sample_json = {
+        "ip":"128.101.101.101",
+        "countries": ["Rwanda", "China", "United States"]
+    }
+    return """<h1>IP Checker: post ip and list of countries to /is_valid to check validity
+    Sample JSON:
+    {
+        "ip":"128.101.101.101",
+        "countries": ["Rwanda", "China", "United States"]
+    }
+    </h1>"""
+
+#Checks if ip address is valid    
+@app.post("/is_valid")
+def is_valid():
+    if request.is_json:
+        data = request.get_json()
+        country = check_address_country(data["ip"])
+        if country == 'Address not in database':
+            return "Not Valid: Address not found in database"
+        if country in data['countries']:
+            return "Valid"
+        else:
+            return "Not Valid: Address not from accepted countries"
+    else:
+        return {"error": "Request must be JSON"}, 415
+
+def check_address_country(ip_add):
+    """Uses geoip database to return IP address country of origin
+
+    Args:
+        ip_add (string): string representation of ip address
+
+    Returns:
+        string: IP address' country of origin
+    """
+    try:
+        response = reader.country(ip_add)
+        return response.country.name
+    except geoip2.errors.AddressNotFoundError:
+        return 'Address not in database'
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
